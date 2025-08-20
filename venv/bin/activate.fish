f5580fc926fba99f507ea94c126f4f23a46d6e12
Shusui <shusui5150@proton.me>
2025-08-20 15:49:16 +0000

    Update basic venv config for the sake of the common user

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/shusui/Documents/scripts/DiscordBot/MeiBot/venv
+set -gx VIRTUAL_ENV /home/shusui/Documents/GitHub/MeiBot/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
